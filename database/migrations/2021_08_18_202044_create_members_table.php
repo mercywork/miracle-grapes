401c2986893ee5ea8a5d465fd525e88de4f651fd
work mercy <mercy.work525@gmail.com>
2021-08-19 12:17:26 +0000

    member model

---
+++ database/migrations/2021_08_18_202044_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('OrderedQty');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('postcode');
+            $table->string('adress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
